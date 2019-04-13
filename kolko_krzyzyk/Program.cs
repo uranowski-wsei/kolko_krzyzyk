f80282e2d8efb416f599a9b0199aaf53f17afeb4
uranowski-wsei <48623716+uranowski-wsei@users.noreply.github.com>
2019-04-13 16:45:14 +0200

    first version of the game

--- kolko_krzyzyk/Program.cs
+++ kolko_krzyzyk/Program.cs
@@ -10,6 +10,22 @@ namespace kolko_krzyzyk
     {
         static void Main(string[] args)
         {
+            string imieGraczaA = "";
+            string imieGraczaB = "";
+            char znakGraczaA = "x";
+            char znakGraczaB = "o";
+            char[,] plansza = new char[3, 3]
+            {
+                {'1','2','3' },
+                {'4','5','6' },
+                {'7','8','9' }
+            };
+
+            Console.Write("Wpisz imie gracza A: ");
+            imieGraczaA = Console.ReadLine();
+            Console.Write("Wpisz imie gracza B: ");
+            imieGraczaB = Console.ReadLine();
         }
+
     }
 }
